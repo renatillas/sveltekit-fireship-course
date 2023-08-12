0b7ad8d3ed4a379640b904128e623ed7b61d3ec6
renatillas <renata.amutio@gmail.com>
2023-08-12 17:14:01 +0200

    feat(login): userstore integrated login and logout

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-  import { auth } from "$lib/firebase";
+  import { auth, user } from "$lib/firebase";
 
-  import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+  import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
@@ -10,7 +10,12 @@
   }
 </script>
 
-<h1>Login</h1>
-<button class="btn btn-primary" on:click={signInWithGoogle}
-  >Sign in with google</button
->
+{#if $user}
+  <h2 class="card-title">Welecome, {$user.displayName}</h2>
+  <p class="text-center text-success">You are logged in</p>
+  <button class="btn btn-warning" on:click={() => signOut(auth)}>Logout</button>
+{:else}
+  <button class="btn btn-primary" on:click={signInWithGoogle}>
+    Sign in with google
+  </button>
+{/if}
