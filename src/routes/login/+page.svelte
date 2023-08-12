08c14d66b1132fd9b6a7b803f1208ccf9fc9c3ad
renatillas <renata.amutio@gmail.com>
2023-08-12 14:16:11 +0200

    feat(login): google basic sign in

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,16 @@
+<script>
+  import { auth } from "$lib/firebase";
+
+  import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+  async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const user = await signInWithPopup(auth, provider);
+    console.log(user);
+  }
+</script>
+
 <h1>Login</h1>
+<button class="btn btn-primary" on:click={signInWithGoogle}
+  >Sign in with google</button
+>
