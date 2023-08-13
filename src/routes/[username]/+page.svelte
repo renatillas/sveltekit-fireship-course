ae987c70d9dabd828d02fbd015b80e270fcdb516
renatillas <renata.amutio@gmail.com>
2023-08-13 22:14:32 +0200

    feat(user page): added

---
+++ src/routes/[username]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<svelte:head>
+  <title>@{data.username} Links</title>
+  <meta name="description" content="Links shared by @{data.username}" />
+</svelte:head>
+
+<main class="prose text-center mx-auto mt-8">
+  <h1 class="text-5xl text-primary my-8">@{data.username}</h1>
+
+  <img class="mx-auto" src={data.photoURL} alt="PhotoURL" width="256" />
+
+  <p class="text-xl my-8">{data.bio}</p>
+
+  <ul class="list-none">
+    {#each data.links as link}
+      <p />{/each}
+  </ul>
+</main>
---
+++ src/routes/[username]/+page.ts
@@ -0,0 +1,23 @@
+import { db } from "$lib/firebase";
+import { error } from "@sveltejs/kit";
+import type { PageLoad } from './$types';
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
+
+export const load = (async ({ params }) => {
+  const collectionRef = collection(db, "users")
+
+  const q = query(collectionRef, where("username", "==", params.username), limit(1))
+  const snapshot = await getDocs(q);
+  const exists = snapshot.docs[0].exists();
+  const data = snapshot.docs[0].data();
+
+  if (!exists) throw error(404, "User not found");
+  if (!data.published) throw error(403, "This profile is not public");
+
+  return {
+    username: data.username,
+    photoURL: data.photoURL,
+    bio: data.bio,
+    links: data.links ?? [],
+  }
+}) satisfies PageLoad;
