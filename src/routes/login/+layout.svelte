2c2463e105332cd5577ff69b0d1e53a92344a83d
renatillas <renata.amutio@gmail.com>
2023-08-13 12:13:24 +0200

    feat: svelte-gestures removed

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,23 +1,6 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
-  import { swipe } from "svelte-gestures";
-
-  function handler(event: any) {
-    let direction = event.detail.direction;
-
-    let currentRouteIndex = routes.indexOf($page.route.id!);
-
-    if (direction === "left") {
-      goto(routes[Math.min(currentRouteIndex + 1, 2)]);
-    }
-    if (direction === "right") {
-      goto(routes[Math.max(currentRouteIndex - 1, 0)]);
-    }
-  }
-
-  let routes = ["/login", "/login/username", "/login/photo"];
 </script>
 
 <nav class="flex justify-center my-6">
@@ -41,11 +24,7 @@
 </nav>
 
 <AnimatedRoute>
-  <main
-    class="card shadow-xl bg-neutral w-5/6 mx-auto"
-    use:swipe={{ touchAction: "pan-y" }}
-    on:swipe={handler}
-  >
+  <main class="card shadow-xl bg-neutral w-5/6 mx-auto">
     <div class="card-body gap-6 items-center text-center">
       <slot />
     </div>
