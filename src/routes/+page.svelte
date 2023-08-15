30dc09d3dfba7f026a794f6be8ab4d682ee91dc9
renatillas <renata.amutio@gmail.com>
2023-08-15 13:30:59 +0200

    feat: Homepage refurbished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { userData } from "$lib/firebase";
+</script>
+
+<svelte:head>
+  <title>Relinks - Just upload your links lol</title>
+  <meta name="description" content="Demo project" />
+</svelte:head>
+
+<main class="flex w-full min-h-screen">
+  <div class="hero bg-base-200">
+    <div class="hero-content text-center">
+      <div class="w-full">
+        <h1 class="text-7xl font-bold">Relinks</h1>
+        <p class="py-6">Just a link multiplexer</p>
+        {#if $userData?.username}
+          <a href="/{$userData?.username}" class="btn btn-primary"
+            >Go to profile</a
+          >
+          <a href="/{$userData?.username}/edit" class="btn btn-primary"
+            >Edit profile</a
+          >
+        {:else}
+          <a href="/login" class="btn btn-primary">Sign Up</a>
+        {/if}
+      </div>
+    </div>
+  </div>
+</main>
